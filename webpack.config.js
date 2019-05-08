4956dab51db1722e650bdfa04b8176bbda2b3f79
chenlin1 <1104654037Qqq.com>
2019-05-08 15:54:27 +0800

    修改

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
-var path = require('path');
-var webpack = require('webpack');
-var VueLoaderPlugin = require('vue-loader/lib/plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
-var MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 module.exports = {
     entry: {
         app: './app/js/main.js',
     },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000
-    },
+    // devServer: {
+    //     contentBase: path.join(__dirname, 'dist'),
+    //     compress: true,
+    //     port: 9000
+    // },
     module: {
         rules: [
             {
@@ -24,66 +26,93 @@ module.exports = {
                 loader: 'vue-loader'
             },
             {
-                test: /\.css$/,
+                test: /\.(sa|sc|c)ss$/,
                 use: [
                     {
-                        loader: "vue-style-loader"
-                    },
-                    // {
-                    //     loader: MiniCssExtractPlugin.loader,
-                    //     options: {
-                    //         hmr: process.env.NODE_ENV === 'development',
-                    //     },
-                    // },
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                          publicPath: '../'
+                        }
+                      },
                     {
-                        loader: "css-loader",
-                        // options: {
-                        //     // 开启 CSS Modules
-                        //     modules: true,
-                        //     // 自定义生成的类名
-                        //     localIdentName: '[local]_[hash:base64:8]'
-                        // }
+                        loader: "css-loader"
                     },
-                    // {
-                    //     loader: 'px2rem-loader',
-                    //     options: {
-                    //         remUni: 75,
-                    //         remPrecision: 8
-                    //     }
-                    // }
+                    {
+                        loader: 'px2rem-loader',
+                        options: {
+                            remUni: 75,
+                            remPrecision: 8
+                        }
+                    },
+                    {
+                        loader: "postcss-loader"
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
                 ]
             },
             {
-                test: /\.scss$/,
-                use: [{
-                    loader: "style-loader" // 将 JS 字符串生成为 style 节点
-                },
-                // {
-                //     loader: MiniCssExtractPlugin.loader,
-                //     options: {
-                //         hmr: process.env.NODE_ENV === 'development',
-                //     },
-                // },
-                {
-                    loader: "css-loader", // 将 CSS 转化成 CommonJS 模块
-                    // options: {
-                    //     // 开启 CSS Modules
-                    //     modules: true,
-                    //     // 自定义生成的类名
-                    //     localIdentName: '[local]_[hash:base64:8]'
-                    // }
-                }, {
-                    loader: "sass-loader" // 将 Sass 编译成 CSS
-                },
-                // {
-                //     loader: 'px2rem-loader',
-                //     options: {
-                //         remUni: 75,
-                //         remPrecision: 8
-                //     }
-                // }
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                          publicPath: '../'
+                        } 
+                    },
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: 'px2rem-loader',
+                        options: {
+                            remUni: 75,
+                            remPrecision: 8
+                        }
+                    },
+                    {
+                        loader: "postcss-loader"
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
                 ]
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      limit: 5000,
+                      // 分离图片至imgs文件夹
+                      name: "imgs/[name].[ext]",
+                    }
+                  },
+                ]
+              },
+              // 图片压缩
+                {
+                    loader: 'image-webpack-loader',
+                    options: {
+                    //   bypassOnDebug: true,
+                    mozjpeg: {
+                        progressive: true,
+                        quality: 65
+                    },
+                    optipng: {
+                        enabled: false,
+                    },
+                    pngquant: {
+                        quality: '65-90',
+                        speed: 4
+                    },
+                    gifsicle: {
+                        interlaced: false,
+                    }
+                    },
+                },
         ]
     },
     plugins: [
@@ -92,9 +121,23 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './app/views/index.html'
         }),
+        new UglifyJsPlugin({
+            uglifyOptions: {
+              compress: {
+                warnings: false, // 去除警告
+                drop_debugger: true, // 去除debugger
+                drop_console: true // 去除console.log
+              },
+            },
+            cache: true, // 开启缓存
+            parallel: true, // 平行压缩
+            sourceMap: false // set to true if you want JS source maps
+          }),
         new MiniCssExtractPlugin({
             filename: 'style.css'
-        })
+        }),
+        new OptimizeCSSAssetsPlugin(),
+
     ],
     resolve: {
         alias: {
